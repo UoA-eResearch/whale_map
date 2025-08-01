f7cbc6ffe56678f00fc6cbc464701c1519ae27f3
Nick Young <nick.young@auckland.ac.nz>
2025-08-01 17:17:46 +1200

    Update Cesium to v1.131, load from cesium.com, remove defunct BingMapsApi key & terrainProvider, format document

--- run_server.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import BaseHTTPServer as bhs
-import SimpleHTTPServer as shs
-
-print('Serving HTTP on 127.0.0.1:8080...')
-bhs.HTTPServer(("127.0.0.1", 8080), shs.SimpleHTTPRequestHandler).serve_forever()
